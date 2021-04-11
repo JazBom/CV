9ae75f35202d17d9b8c01e5ca527a3b8a3fcb0db
Jessica Bommer <xininu@gmail.com>
2021-04-11 18:30:31 +1000

    improve accessibility with img alt tags

--- src/App.js
+++ src/App.js
@@ -6,11 +6,18 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faPalette, faEnvelope, faPaperPlane, faPhone, faPhoneAlt, faMobile, faMobileAlt, faFileDownload, faArrowAltCircleDown, faArrowDown, faGem, faDatabase } from '@fortawesome/free-solid-svg-icons'
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+  TransitionGroup,
+  CSSTransition
+} from "react-transition-group";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useLocation,
+  useParams
+} from "react-router-dom";
 import { Nav } from './components/Nav';
 import { Banner } from './components/Banner';
 import { Container } from './components/Container';
@@ -26,12 +33,14 @@ const App = () => {
         <div className="App">
         
         {/* <img src="https://jessicabommerblog.files.wordpress.com/2017/02/original-1.jpg" className="img-background"/>  */}
-        <img src="../Assets/BannerPic.png" className="img-background"/>  
+        <img src="../Assets/BannerPic.png" className="img-background" alt="watercolor paint strokes"/>  
 
           
           <Nav/>
           <Banner/>
-           
+          {/* <CSSTransition
+            classNames="fade"
+            timeout={300}> */}
             <Switch>
 
             <Route exact path="/art">
@@ -42,8 +51,13 @@ const App = () => {
               <Container/>
             </Route>
 
+            
+            {/* <Route path="*">
+              <AnimationApp/>
+            </Route> */}
+            
             </Switch>
-
+            {/* </CSSTransition> */}
           <Footer/>
 
         </div>
